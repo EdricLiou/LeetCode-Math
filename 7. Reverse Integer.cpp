2425e542ebf420d0158ff54da59965698b924886
EdricLiou <101196703+EdricLiou@users.noreply.github.com>
2022-09-18 19:34:26 +0800

    Create 7. Reverse Integer.cpp

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,30 @@
+//method 1
+
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            if (abs(res) > INT_MAX / 10) return 0;
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return res;
+    }
+};
+
+//method 2
+
+class Solution {
+public:
+    int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = 10 * res + x % 10;
+            x /= 10;
+        }
+        return (res > INT_MAX || res < INT_MIN) ? 0 : res;
+    }
+};
+
+//https://www.cnblogs.com/grandyang/p/4125588.html
